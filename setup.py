c00faa2ec5c5f192d6454ddb0380a0e9a08871f4
Fabio Takaki <fabio@takaki.me>
2017-08-19 04:15:22 -0300

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='app_creditcard',
+    packages=['app_creditcard'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
