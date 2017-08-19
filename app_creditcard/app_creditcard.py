53e6076ead4d942ba191180b5034af06bf6132cd
Fabio Takaki <fabio@takaki.me>
2017-08-19 04:53:48 -0300

    changes for heroku

--- app_creditcard/app_creditcard.py
+++ app_creditcard/app_creditcard.py
@@ -2,15 +2,15 @@ import os
 from flask import Flask, request, g, redirect, url_for, abort, render_template, jsonify
 from creditcard import CreditCard
 
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app_creditcard = Flask(__name__)
+app_creditcard.config.from_object(__name__)
+app_creditcard.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-@app.route('/', methods=['GET'])
+@app_creditcard.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
 
-@app.route('/validate', methods=['POST'])
+@app_creditcard.route('/validate', methods=['POST'])
 def validate():
   assert request.path == '/validate'
   assert request.method == 'POST'
